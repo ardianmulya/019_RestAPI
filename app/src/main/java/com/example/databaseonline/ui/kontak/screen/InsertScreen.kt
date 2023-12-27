089e0d5c1616887ddeb641767b72f9753eb80cc9
ardianmulya <ardian.mulya7@gmail.com>
2023-12-27 20:58:21 +0700

    menambah fun EntryKontakScreen

--- app/src/main/java/com/example/databaseonline/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/screen/InsertScreen.kt
@@ -4,19 +4,82 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.databaseonline.navigation.DestinasiNavigasi
 import com.example.databaseonline.ui.kontak.viewmodel.InsertUiEvent
 import com.example.databaseonline.ui.kontak.viewmodel.InsertUiState
+import com.example.databaseonline.ui.kontak.viewmodel.InsertViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.databaseonline.ui.PenyediaViewModel
+import com.example.databaseonline.ui.TopAppBar
+import com.example.databaseonline.ui.halaman.DetinasiHome
+import com.example.databaseonline.ui.halaman.HomeStatus
+import kotlinx.coroutines.launch
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleres = "Entry Siswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = DetinasiHome.titleres,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+    ) { innerPadding ->
+
+        EntryKontakBody(
+            insertUiState = viewModel.insertKontakState,
+            onKontakValueChange = viewModel::updateInsertKontakState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+            )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
