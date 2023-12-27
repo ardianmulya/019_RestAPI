06a8a411ce7af005645d3c099d89dd9d51f07026
ardianmulya <ardian.mulya7@gmail.com>
2023-12-27 20:11:00 +0700

    menambah data class

--- app/src/main/java/com/example/databaseonline/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.databaseonline.ui.kontak.viewmodel
 
-class InsertViewModel {
+import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.databaseonline.repositori.KontakRepositori
+
+class InsertViewModel (private val kontakRepositori: KontakRepositori):ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 }
+
+data class  InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama : String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
