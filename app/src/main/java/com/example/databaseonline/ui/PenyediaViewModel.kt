f8da77a4c2628131dcf45754bdf8c1c1722d3767
ardianmulya <ardian.mulya7@gmail.com>
2023-12-27 20:21:43 +0700

    menambah InsertViewModel

--- app/src/main/java/com/example/databaseonline/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/PenyediaViewModel.kt
@@ -7,12 +7,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.databaseonline.KontakApplication
+import com.example.databaseonline.ui.kontak.viewmodel.InsertViewModel
 import com.example.databaseonline.ui.view_model.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(aplikasiMars().container.kontakRepositori)
+        }
+        initializer {
+            InsertViewModel(aplikasiMars().container.kontakRepositori)
         }
     }
 }
