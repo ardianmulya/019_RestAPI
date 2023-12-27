5a5ae714b877cc22e127086e11c1e56fd89d8bd5
ardianmulya <ardian.mulya7@gmail.com>
2023-12-27 20:47:37 +0700

    menambah fun EntryKontakBody

--- app/src/main/java/com/example/databaseonline/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/screen/InsertScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.databaseonline.ui.kontak.viewmodel.InsertUiEvent
+import com.example.databaseonline.ui.kontak.viewmodel.InsertUiState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,14 +25,14 @@ fun FormInputSiswa(
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
-){
+) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         OutlinedTextField(
             value = insertUiEvent.nama,
-            onValueChange ={onValueChange(insertUiEvent.copy(nama = it))},
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
             label = { Text("Nama") },
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
@@ -38,7 +41,7 @@ fun FormInputSiswa(
 
         OutlinedTextField(
             value = insertUiEvent.email,
-            onValueChange ={onValueChange(insertUiEvent.copy(email = it))},
+            onValueChange = { onValueChange(insertUiEvent.copy(email = it)) },
             label = { Text("Email") },
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
@@ -47,7 +50,7 @@ fun FormInputSiswa(
 
         OutlinedTextField(
             value = insertUiEvent.nohp,
-            onValueChange ={onValueChange(insertUiEvent.copy(nohp = it))},
+            onValueChange = { onValueChange(insertUiEvent.copy(nohp = it)) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text("Telepon") },
             singleLine = true,
@@ -59,5 +62,31 @@ fun FormInputSiswa(
             thickness = 8.dp,
             modifier = Modifier.padding(bottom = 12.dp)
         )
+    }
+}
+
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onKontakValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputSiswa(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onKontakValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
