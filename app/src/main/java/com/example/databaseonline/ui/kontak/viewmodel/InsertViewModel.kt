b6601870500ee1082b5c411fcde7ea5f810f6f08
ardianmulya <ardian.mulya7@gmail.com>
2023-12-27 20:19:51 +0700

    menambah supend fun

--- app/src/main/java/com/example/databaseonline/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/viewmodel/InsertViewModel.kt
@@ -5,12 +5,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.databaseonline.modeldata.Kontak
 import com.example.databaseonline.repositori.KontakRepositori
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val kontakRepositori: KontakRepositori):ViewModel(){
     var insertKontakState by mutableStateOf(InsertUiState())
         private set
+
+    fun updateInsertKontakState(insertUiEvent : InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepositori.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class  InsertUiState(
